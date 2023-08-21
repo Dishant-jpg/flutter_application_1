c72c49f2f9f2a1fde4f691b4576073b777913b01
Dishant-jpggit config --global user.name Dishant-jpggit config --global user.email gupadishant90@gmail.comgit config --global user.name Dishant-jpg <84134946+Dishant-jpg@users.noreply.github.com>
2023-08-21 19:01:56 +0530

    tter

--- lib/calculator making/custom/wallapar.dart
+++ lib/calculator making/custom/wallapar.dart
@@ -2,24 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/calculator%20making/custom/custom_widget.dart';
 
 class Mywallapars extends StatefulWidget {
-  const Mywallapars({super.key});
-
   @override
   State<Mywallapars> createState() => _MywallaparsState();
 }
 
+
 class _MywallaparsState extends State<Mywallapars> {
+  TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Column(
               children: [
-                SizedBox(
-                  height: 30,
-                )
+                Uihelper.customtextfield(
+                    "Email", Icons.email, emailController, false)
               ],
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_1/Practise_Task/calculator.dart';
 import 'package:flutter_application_1/Practise_Task/wall1.dart';
 import 'package:flutter_application_1/Practise_Task/wallapar.dart';
 import 'package:flutter_application_1/animation.dart';
+import 'package:flutter_application_1/calculator%20making/custom/wallapar.dart';
 import 'package:flutter_application_1/calculator%20making/first_page.dart';
 import 'package:flutter_application_1/cicrle_avatara.dart';
 import 'package:flutter_application_1/login_design/loginsubmit.dart';
@@ -11,7 +12,7 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyAnimation(),
+      home: Mywallapars(),
     ),
   );
 }
